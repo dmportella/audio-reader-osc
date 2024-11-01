00f915290422492af18797a539d2ac4f72122a58
ameliend <a.deshams+git@slmail.me>
2024-11-01 16:30:53 +0100

    test: add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 """Pytest configuration file for Audio Reader OSC."""
 
+from pathlib import Path
+
 import pytest
+
+CURRENT_DIR = Path(__file__).parent
 
 
 def pytest_configure(config):
@@ -8,11 +12,22 @@ def pytest_configure(config):
     # Add `@pytest.mark.local` above your tests functions you want to ignore online
     config.addinivalue_line("markers", "local : mark a local test")
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://gitlab.com/ameliend/python-package-cookiecutter-template')
+@pytest.fixture
+def test_folder_location():
+    return CURRENT_DIR / "test_folder"
+
+
+@pytest.fixture
+def test_folder_with_no_audio_files_location():
+    return CURRENT_DIR / "test_folder_with_no_audio_files"
+
+
+@pytest.fixture
+def test_folder_with_no_json_location():
+    return CURRENT_DIR / "test_folder_with_no_json"
+
+
+@pytest.fixture
+def test_folder_with_missing_audio_files():
+    return CURRENT_DIR / "test_folder_with_missing_audio_files"
--- tests/test_audio_reader_osc.py
+++ tests/test_audio_reader_osc.py
@@ -1,18 +1,27 @@
 """Tests for Audio Reader OSC."""
 
-import pytest
+from pathlib import Path
+
+from audio_reader_osc import pick_random_audio_file_with_text
 
 
-def test_content(response):
-    """Sample pytest test function with a pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitLab' in BeautifulSoup(response.content).title.string
+def test_pick_random_audio_file_with_text(test_folder_location):
+    filepath, text = pick_random_audio_file_with_text(test_folder_location)
+    filepath = str(Path(filepath).absolute())
+    expected_pairs = [
+        (str(test_folder_location / "16194894.mp3"), "Trust me, I'm not an AI"),
+        (str(test_folder_location / "23919904.mp3"), "Am I really an AI?"),
+    ]
+    assert (filepath, text) in expected_pairs
 
-@pytest.mark.local
-def test_content_local(response):
-    """Sample pytest test function with a pytest local" marker.
 
-    This pytest marker can be added to your test functions to ignore this test in our GitLab pipelines.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitLab' in BeautifulSoup(response.content).title.string
+def test_pick_random_audio_file_with_text_no_audio(test_folder_with_no_audio_files_location):
+    assert pick_random_audio_file_with_text(test_folder_with_no_audio_files_location) is None
+
+
+def test_pick_random_audio_file_with_text_no_json(test_folder_with_no_json_location):
+    assert pick_random_audio_file_with_text(test_folder_with_no_json_location) is None
+
+
+def test_pick_random_audio_file_with_missing_audio(test_folder_with_missing_audio_files):
+    assert pick_random_audio_file_with_text(test_folder_with_missing_audio_files)
